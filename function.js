faaad4a8b038be7afc10f515a05371a7f5d40275
Sonqo-Toconas <126425001+Sonqo-Toconas@users.noreply.github.com>
2023-03-07 19:57:18 -0300

    Add files via upload

---
+++ function.js
@@ -0,0 +1,8 @@
+document.getElementById("boton1").onclick = function () {
+    document.getElementById("demo1").style.display = "block"
+    document.getElementById("demo2").style.display = "block"
+}
+document.getElementById("boton2").onclick = function () {
+    document.getElementById("demo1").style.display = "none";
+    document.getElementById("demo2").style.display = "none";
+}
